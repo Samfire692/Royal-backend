b8bf2cad92033886531d8e85e3c485a06e0897f0
unknown <akinsolasamuel372@gmail.com>
2026-02-24 03:49:26 +0100

    Royal-backend

---
+++ router/authrouter.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const route = express.Router();
+const db = require("../database/database");
+
+route.post("/signup", async(req,res)=> {
+    try{
+      const {fullname, email, password} = req.body;
+
+      const {data, error:authError} = await db.auth.signUp({
+        email,
+        password
+      })
+
+      if(authError) return res.status(400).json({error:authError.message});
+
+     const {error:tableError} = await db.from("royalsignup")
+     .insert({
+        id:data.user.id,
+        fullname,
+        email
+     })
+
+     if(tableError) return res.status(400).json({error:tableError.message});
+
+     res.status(201).json({message:"Account created successfully, please check your email for confirmation!"});
+
+    }catch(error) {return res.status(500).json({error:error.message})}
+})
+
+route.post("/login", async(req, res)=>{
+  try{
+     const {email, password}= req.body;
+     const {data:authData, error} = await db.auth.signInWithPassword({
+       email,
+       password
+     })
+
+     if(error) {
+
+        if(error.message == "Email not confirmed"){
+         res.status(400).json({error:"Check your email for Confirmation"})
+         return;
+      }
+
+        return res.status(400).json({error:error.message});
+    }
+
+   const {data:tableData} = await db.from("royalsignup")
+       .select("*")
+       .eq("id", authData.user.id)
+       .single()
+      
+
+      res.json({
+        message:"Login Successful",
+        user:{
+            id:tableData.id,
+            email:tableData.email,
+            name:tableData.name
+        },
+        session:authData.session
+      })
+
+   }catch(error){return res.status(500).json({error:error.message})}
+})
+
+module.exports= route;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const db = require("./database/database")
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT
+const auth_routes = require("./router/authrouter");
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/authrouter', auth_routes)
+
+app.get("/" , (req,res)=> {
+    res.send("app running")
+})
+
+app.listen(PORT, ()=> {
+    console.log(`listening on port http://127.0.0.1:${PORT}`)
+})
